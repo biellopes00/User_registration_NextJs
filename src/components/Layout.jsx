1fd307b624bff267ef94f2bc39610a42999c63b0
Gabriel Lopes <gabriellopes031997@hotmail.com>
2024-01-01 09:45:37 -0300

    Creating layout component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+import styles from '../styles/Layout.module.css'
+
+export default function Layout(props){
+    return(
+        <div className={styles.layout}>
+            <div className={styles.header}>
+                <h1>{props.title}</h1>
+            <Link href="/">Back</Link>
+                
+            </div>
+            <div className={styles.content}>
+                {props.children}
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+export default function Navigation(){
+    return(
+        <Link href="/">
+        
+        </Link>
+    )
+}
